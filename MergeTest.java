0e891f641764e34814f6a91814d5d3c19f5dc790
WZJun <128673164+OSshiyanGuoGuoGuo@users.noreply.github.com>
2024-08-07 10:53:41 +0800

    Add files via upload

---
+++ MergeTest.java
@@ -0,0 +1,9 @@
+public class MergeTest {
+    public void doSomethingMain() {
+        System.out.println("Doing something in main-branch!");
+    }
+
+    public void anotherMethodMain() {
+        System.out.println("Another method in main-branch!");
+    }
+}
